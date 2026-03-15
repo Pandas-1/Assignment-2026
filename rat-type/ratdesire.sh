ea8ccbf34635f44afc77b4bc96cac46460a6f271
Pandas-1 <khemkavarun11@gmail.com>
2026-03-15 22:32:42 +0530

    refactor: added TUI for the typing

--- rat-type/ratdesire.sh
+++ rat-type/ratdesire.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # https://www.youtube.com/watch?v=4EA07LUEMZI
-# 
+RED='\e[31m'
+GREEN='\e[32m'
+ENDCOLOR='\e[0m' 
 echo "You give a man a rat, you satisfy his RAT DESIRE. You give a man a keyboard, you satisfy his RAT DESIRE."
 echo "When ready to type, write "y" and press ENTER."
 echo "The timer starts immediately when you press "y"."
@@ -25,15 +27,59 @@ echo "TEXT: ${quote}"
 echo ""
 read -n 1 -p "Are you ready to start? [y/n]: " response
 echo ""
-
+incorrect_char=0
 
 
 len=${#quote}
 len_words_quote=${#words_quote[@]}
 if [ ${response} == "y" ]; then
   initial=${EPOCHREALTIME}
-  read -e -n ${len} -p "PROMPT: " input # restricted input length
-  diff=$(echo "${EPOCHREALTIME} - ${initial}" | bc)
+    
+  len=${#quote}
+  typed=""
+  i=0
+  mistake=false
+
+  while true; do
+    tput clear
+    tput cup 0 0
+
+    for ((j=0; j<${#typed}; j++)); do
+      c="${typed:j:1}"
+      q="${quote:j:1}"
+      if [[ "$c" == "$q" ]]; then
+        echo -ne "${GREEN}${c}${ENDCOLOR}"
+      else
+        echo -ne "${RED}${q}${ENDCOLOR}"
+      fi
+    done  
+
+
+    remaining="${quote:${#typed}}"
+    echo -ne "${remaining}"
+
+
+    if [[ ${#typed} -ge ${#quote} ]]; then
+      tput cup 1 0
+      for ((j=0; j<${#quote}; j++)); do
+        if [[ "${typed:j:1}" != "${quote:j:1}" ]]; then
+           ((incorrect_char++))     
+        fi
+      done
+      break
+    fi
+    IFS= read -r -s -n 1 temp
+
+    if [[ $temp == $'\x7f' || $temp == $'\b' ]]; then
+      # Backspace
+      if [[ ${#typed} -gt 0 ]]; then
+        typed="${typed%?}"
+      fi
+    else
+      typed+="$temp"
+    fi
+  done
+   diff=$(echo "${EPOCHREALTIME} - ${initial}" | bc)
   # echo ${diff}
 else
   exit
@@ -42,16 +88,7 @@ fi
 
 read -r  -a words_input <<< "$input"
 # compare quote and input
-errornum=0
 wordcnt=0
-for (( i=0; i<${len}; i++ )); do
-  char1=${quote:$i:1}
-  char2=${input:$i:1}
-
-  if [[ "${char1}" != "${char2}" ]]; then
-    errornum=$((errornum+1))
-  fi
-done
 correct_words=0
 #echo "${words_input[@]}"
 #echo "${words_quote[@]}"
@@ -60,12 +97,10 @@ for((i=1; i < $(($len_words_quote+1)); i++)); do
      correct_words=$(($correct_words+1))
   fi
 done
-echo $correct_words
 # echo ${errornum}
-
 # unadjusted wpm calculation
 wpm=$(echo "scale=2; (${len}*12)/${diff}" | bc)
-acc=$(echo "scale=2; 100 *  $correct_words / $len_words_quote" | bc)
+acc=$(echo "scale=2; 100 * ($len - $incorrect_char) / $len " | bc)
 
 # crunch the numbers
 echo "Raw WPM: ${wpm}"
