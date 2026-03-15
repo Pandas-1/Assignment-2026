9a859f490d50537b1703c133833514a65b342f3a
Pandas-1 <khemkavarun11@gmail.com>
2026-03-15 15:35:15 +0530

    refactor: using word by word comparision instead of character by character for accuracy

--- rat-type/ratdesire.sh
+++ rat-type/ratdesire.sh
@@ -3,7 +3,7 @@
 # 
 echo "You give a man a rat, you satisfy his RAT DESIRE. You give a man a keyboard, you satisfy his RAT DESIRE."
 echo "When ready to type, write "y" and press ENTER."
-echo "The timer starts immediately when you press ENTER."
+echo "The timer starts immediately when you press "y"."
 echo "When you are done, press ENTER immediately."
 #insert part for difficulty selection
 
@@ -14,10 +14,12 @@ fi
 play_again=y
 while [ "$play_again" == "y" ]; do 
 quote=$(shuf -n 1 $1)
-while [ "${qoute}" == "" ]
+while [ "${quote}" == "" ]
 do
-  qoute=$(shuf -n 1 $1)
+  quote=$(shuf -n 1 $1)
 done
+
+read -r  -a words_quote <<< "$quote"
 echo ""
 echo "TEXT: ${quote}"
 echo ""
@@ -27,7 +29,7 @@ echo ""
 
 
 len=${#quote}
-
+len_words_quote=${#words_quote[@]}
 if [ ${response} == "y" ]; then
   initial=${EPOCHREALTIME}
   read -e -n ${len} -p "PROMPT: " input # restricted input length
@@ -37,6 +39,8 @@ else
   exit
 fi
 
+
+read -r  -a words_input <<< "$input"
 # compare quote and input
 errornum=0
 wordcnt=0
@@ -48,11 +52,20 @@ for (( i=0; i<${len}; i++ )); do
     errornum=$((errornum+1))
   fi
 done
+correct_words=0
+#echo "${words_input[@]}"
+#echo "${words_quote[@]}"
+for((i=1; i < $(($len_words_quote+1)); i++)); do
+  if [[ "${words_quote[$i]}" == "${words_input[$i]}" ]]; then
+     correct_words=$(($correct_words+1))
+  fi
+done
+echo $correct_words
 # echo ${errornum}
 
 # unadjusted wpm calculation
 wpm=$(echo "scale=2; (${len}*12)/${diff}" | bc)
-acc=$(echo "scale=2; 100-(${errornum}*100/${len})" | bc)
+acc=$(echo "scale=2; 100 *  $correct_words / $len_words_quote" | bc)
 
 # crunch the numbers
 echo "Raw WPM: ${wpm}"
